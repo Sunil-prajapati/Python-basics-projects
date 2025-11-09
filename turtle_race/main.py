2b51d1f17cbcb0a27eb64f2fa9656b7dc0973ff7
sunilSupertal <sunil@supertal.io>
2025-11-09 12:01:25 +0530

    turtle and slot machine

---
+++ turtle_race/main.py
@@ -0,0 +1,59 @@
+import turtle
+import time
+import random
+
+WIDTH, HEIGHT = 500, 500
+COLORS = ['red','green','blue','orange','black','yellow','purple','pink','brown','cyan']
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input('Enter the number of racers (2 - 10): ')
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is nor numeric... Try Again!')
+            continue
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Number not in range 2-10. Try Again!")
+
+def race(colors):
+    turtles = create_turtle(colors)
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+            
+            x,y = racer.pos()
+            if y >= HEIGHT // 2 -10:
+                return colors[turtles.index(racer)]
+    
+
+def create_turtle(colors):
+    turtles = []
+    spacingX = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):    #give index and value both by using enumerate
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i+1) * spacingX, - HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+    return turtles
+    
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle racing")
+racers = get_number_of_racers()
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]  #WORK AS SLICE
+winner = race(colors)
+print("The Winner is the turtle with color:", winner)
+time.sleep(5)
